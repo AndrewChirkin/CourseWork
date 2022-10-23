7fdad528a123b36d1c66ec09ffd22ba8fdd63c31
Andrew <chirkash11@gmail.com>
2022-10-23 18:05:55 +0300

    try1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee IvanovIvanIvanovich = new Employee("Иванов", "Иван", "Иванович");
+        Employee PetrovPetrPetrovich = new Employee("Петров","Пётр", "Петрович");
+        Employee AlekseevAlekseyAlekseevich = new Employee("Алексеев","Алексей","Алексеевич");
+        System.out.println(Employee.printAllEmployeesName());
     }
 }
